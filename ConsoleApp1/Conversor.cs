75a14d19c7a57a097715b4ba0b9248537535cf67
jeferson Ferreira <jeferson.ferrreira@etecjk.com.br>
2026-04-01 17:40:30 -0300

    Exercicio 7

---
+++ ConsoleApp1/Conversor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio7
+{
+    class Conversor
+    {
+        double dolar, real;
+        double total;
+
+        public void solicitarDolar()
+        {
+            Console.WriteLine("Digite o valor da cotação do Dolar($)");
+            dolar = double.Parse(Console.ReadLine());
+
+        }
+        public void solicitarReal()
+        {
+            Console.WriteLine("Digite o valor da cotação do Real(R$)");
+            real = double.Parse(Console.ReadLine());
+
+        }
+
+        public void  converter() {
+            total = dolar * real;
+        }
+        public void mostrar()
+        {
+            Console.WriteLine("Total em Reais R${0}",total);
+        }
+    }
+}
