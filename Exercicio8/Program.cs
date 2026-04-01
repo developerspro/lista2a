0283939607bb2beec34494a446512681c42615a9
jeferson Ferreira <jeferson.ferrreira@etecjk.com.br>
2026-04-01 17:50:26 -0300

    Exercicio 8

---
+++ Exercicio8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao posicao = new Posicao();
+            Console.WriteLine("Digite um numero");
+            posicao.alterarPosicao(int.Parse(Console.ReadLine()));
+        }
+    }
+}
