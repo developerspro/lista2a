b2cbb80979f524c04a132be0632071419a5cc49f
jeferson Ferreira <jeferson.ferrreira@etecjk.com.br>
2026-04-01 17:11:06 -0300

    exercicio 5

---
+++ CA0104/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+ * 
+ * 
+5. Conversor de Temperatura (Celsius para Fahrenheit)
+Descrição: Solicite uma temperatura em Celsius e converta para Fahrenheit.
+Fórmula: F = C * 9/5 + 32
+ * 
+ * 
+ */
+namespace CA0104
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Termometro term = new Termometro();
+            term.solicitarTempCelsius();
+            term.converter();
+            term.exibirConversao();
+        }
+    }
+}
---
+++ CA0104/Termometro.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA0104
+{
+     class Termometro
+    {
+        public double celsius;
+        public double fahrenheit;
+
+        public void solicitarTempCelsius()
+        {
+            Console.WriteLine("Digite a temperatura em Celsius");
+            celsius = double.Parse(Console.ReadLine());
+        }
+
+        public void converter()
+        {
+            //Fórmula: F = C * 9/5 + 32
+            fahrenheit = celsius * 9/5 + 32;
+        }
+
+        public void exibirConversao()
+        {
+            Console.WriteLine("Celsius:{0} em Fahrenheit:{1}",celsius,fahrenheit);
+        }
+
+    }
+}
