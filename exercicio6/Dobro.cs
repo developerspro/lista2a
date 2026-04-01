d26894b199cecebaab97830260863df21ea8877e
jeferson Ferreira <jeferson.ferrreira@etecjk.com.br>
2026-04-01 17:26:12 -0300

    Exercicio 6

---
+++ exercicio6/Dobro.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio6
+{
+    internal class Dobro
+    {
+        double numero;
+        double dobro;
+
+        public void solicitarNumero()
+        {
+            Console.WriteLine("Digite um numero para calcular o Dobro");
+            numero = double.Parse(Console.ReadLine());
+        }
+
+        public void calcularDobro()
+        {
+            dobro = numero * 2;
+        }
+
+        public void mostrarResultado()
+        {
+            Console.WriteLine("numero:{0} e seu dobro{1}",numero,dobro);
+        }
+    }
+}
---
+++ exercicio6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dobro db = new Dobro();
+            db.solicitarNumero();
+            db.calcularDobro();
+            db.mostrarResultado();
+        }
+    }
+}
